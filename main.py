5dc0ef1b352c45bea4096e37555d996111c668fd
Matthew <matthew6303@gmail.com>
2025-05-15 23:21:24 +0800

    update port

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import gradio as gr
 def greet(name):
     return f"Hello {name}!"
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text").launch()
+iface = gr.Interface(fn=greet, inputs="text", outputs="text").launch(server_name="0.0.0.0", server_port=7860)
